b11b84eb8e96d88b42cb9b3336978e78e5bcc936
Ammar Ramees <ammarramees@gmail.com>
2022-11-14 22:16:19 +0530

    route changes

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,12 +1,15 @@
 import RoundedButton from "../../Components/Rounded Button/RoundedButton";
 import Navbar from "../../Components/Navigation/Navbar";
+import { Link } from "react-router-dom";
 
 function About () {
     return (
         <div>
             {/* <Navbar text="Home"/> */}
             <h1> This is About Page</h1>
+            <Link to={"/"}>
             <RoundedButton text="click to go Home"/>
+            </Link>
         </div>
     );
 }
